85e4962179d2ebb7581e4e279cf501675970fc60
Stephen Hack <stephen.hack@gmail.com>
2019-10-24 10:17:20 -0400

    Added CORS

--- src/app.js
+++ src/app.js
@@ -5,15 +5,28 @@ const app = express();
 
 const videos = require('./lib/videos');
 
+app.use((req, res, next) => {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader(
+  'Access-Control-Allow-Header',
+  'Origin, X-Requested-With, Content-Type, Accept'
+ );
+ res.setHeader(
+  'Access-Control-Allow-Methods',
+  'GET, POST, PATCH, DELETE, OPTIONS'
+ );
+ next();
+});
+
 app.use('/api/videos', (req, res, next) => {
-  res.status(200).json({
-    message: 'Video Data fetched successfully!',
-    videos: videos
-  })
-})
+ res.status(200).json({
+  message: 'Video Data fetched successfully!',
+  videos: videos
+ });
+});
 
 app.use((req, res, next) => {
-  res.send('Hello from Express');
+ res.send('Hello from Express');
 });
 
 module.exports = app;
--- src/lib/videos.js
+++ src/lib/videos.js
@@ -3,42 +3,48 @@
 const videos = [
  {
   career: 'Actuary',
-  date: 'Oct 22, 2019',
+  created: 'Oct 22, 2019',
+  id: null,
   link: 'https://youtu.be/4OFa1BeAVvo',
   title: 'Actuary Salary (2019) - How much do actuaries make',
   type: 'Salary'
  },
  {
   career: 'Actuary',
-  date: 'Oct 21, 2019',
+  created: 'Oct 21, 2019',
+  id: null,
   link: 'https://youtu.be/PSMMmdadSJg',
   title: 'Actuary Jobs (2019) - Top 5 Places',
   type: 'Jobs'
  },
  {
   career: 'Electrician',
-  date: 'Oct 18, 2019',
+  created: 'Oct 18, 2019',
+  id: null,
   link: 'https://youtu.be/Fk-dsICby10',
   title: 'Electrician Salary (2019) - How much do electricians make',
   type: 'Salary'
  },
  {
   career: 'Electrician',
-  date: 'Oct 17, 2019',
+  created: 'Oct 17, 2019',
+  id: null,
   link: 'https://youtu.be/3YbuzeKGyA4',
   title: 'Electrician Jobs (2019) - How many electricians jobs are there',
   type: 'Jobs'
  },
  {
   career: 'Architect',
-  date: 'Oct 15, 2019',
+  created: 'Oct 15, 2019',
+  id: null,
   link: 'https://youtu.be/jj6HrLV30N0',
   title: 'Architect Salary (2019) - How much do architects make',
   type: 'Salary'
  },
  {
   career: 'Architect',
-  date: 'Oct 14, 2019',
+  created: 'Oct 14, 2019',
+  id: null,
   link: 'https://youtu.be/mZ4Rlh9U-WY',
   title: 'Architect Jobs (2019) - Top 5 Places',
   type: 'Jobs'
