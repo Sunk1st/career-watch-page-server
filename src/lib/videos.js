a467f4076f27dbe45f1250037c444b1b34e7e515
Stephen Hack <stephen.hack@gmail.com>
2019-10-24 15:25:10 -0400

    updated data on server

--- src/lib/videos.js
+++ src/lib/videos.js
@@ -5,7 +5,7 @@ const videos = [
   career: 'Actuary',
   created: 'Oct 22, 2019',
   id: null,
-  link: 'https://youtu.be/4OFa1BeAVvo',
+  link: 'https://www.youtube.com/embed/4OFa1BeAVvo',
   title: 'Actuary Salary (2019) - How much do actuaries make',
   type: 'Salary'
  },
@@ -13,7 +13,7 @@ const videos = [
   career: 'Actuary',
   created: 'Oct 21, 2019',
   id: null,
-  link: 'https://youtu.be/PSMMmdadSJg',
+  link: 'https://www.youtube.com/embed/PSMMmdadSJg',
   title: 'Actuary Jobs (2019) - Top 5 Places',
   type: 'Jobs'
  },
@@ -21,7 +21,7 @@ const videos = [
   career: 'Electrician',
   created: 'Oct 18, 2019',
   id: null,
-  link: 'https://youtu.be/Fk-dsICby10',
+  link: 'https://www.youtube.com/embed/Fk-dsICby10',
   title: 'Electrician Salary (2019) - How much do electricians make',
   type: 'Salary'
  },
@@ -29,7 +29,7 @@ const videos = [
   career: 'Electrician',
   created: 'Oct 17, 2019',
   id: null,
-  link: 'https://youtu.be/3YbuzeKGyA4',
+  link: 'https://www.youtube.com/embed/3YbuzeKGyA4',
   title: 'Electrician Jobs (2019) - How many electricians jobs are there',
   type: 'Jobs'
  },
@@ -37,7 +37,7 @@ const videos = [
   career: 'Architect',
   created: 'Oct 15, 2019',
   id: null,
-  link: 'https://youtu.be/jj6HrLV30N0',
+  link: 'https://www.youtube.com/embed/jj6HrLV30N0',
   title: 'Architect Salary (2019) - How much do architects make',
   type: 'Salary'
  },
@@ -45,7 +45,7 @@ const videos = [
   career: 'Architect',
   created: 'Oct 14, 2019',
   id: null,
-  link: 'https://youtu.be/mZ4Rlh9U-WY',
+  link: 'https://www.youtube.com/embed/mZ4Rlh9U-WY',
   title: 'Architect Jobs (2019) - Top 5 Places',
   type: 'Jobs'
  }
