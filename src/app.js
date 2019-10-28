7fb10ca7d84d2ec2bd1c23eaeff4e69d3221fc82
Stephen Hack <stephen.hack@gmail.com>
2019-10-28 13:50:06 -0400

    added CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,28 @@
 const path = require('path');
 const express = require('express');
 
+const videoData = require('./lib/video-data');
+
 const app = express();
+
+app.use((req, res, next) => {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader(
+  'Access-Control-Allow-Header',
+  'Origin, X-Requested-With, Content-Type, Accept'
+ );
+ res.setHeader(
+  'Access-Control-Allow-Methods',
+  'GET, POST, PATCH, DELETE, OPTIONS'
+ );
+ next();
+});
+
+app.use('/api/videos', (req, res, next) => {
+ res.status(200).json({
+  message: 'Videos fetched successfully',
+  videoData: videoData
+ });
+});
+
+module.exports = app;
---
+++ src/lib/video-data.js
@@ -0,0 +1,36 @@
+const videoData = [
+ {
+  career: 'Actuary',
+  created: 'Oct 22, 2019',
+  id: '4OFa1BeAVvo',
+  link: 'https://www.youtube.com/embed/4OFa1BeAVvo',
+  title: 'Actuary Salary (2019) - How much do actuaries make',
+  type: 'Salary'
+ },
+ {
+  career: 'Actuary',
+  created: 'Oct 21, 2019',
+  id: 'PSMMmdadSJg',
+  link: 'https://www.youtube.com/embed/PSMMmdadSJg',
+  title: 'Actuary Jobs (2019) - Top 5 Places',
+  type: 'Jobs'
+ },
+ {
+  career: 'Electrician',
+  created: 'Oct 18, 2019',
+  id: 'Fk-dsICby10',
+  link: 'https://www.youtube.com/embed/Fk-dsICby10',
+  title: 'Electrician Salary (2019) - How much do electricians make',
+  type: 'Salary'
+ },
+ {
+  career: 'Electrician',
+  created: 'Oct 17, 2019',
+  id: '3YbuzeKGyA4',
+  link: 'https://www.youtube.com/embed/3YbuzeKGyA4',
+  title: 'Electrician Jobs (2019) - How many electricians jobs are there',
+  type: 'Jobs'
+ }
+];
+
+module.exports = videoData;
