5901f5bb4685c88d0b6d7803988ce19733620e90
Stephen Hack <stephen.hack@gmail.com>
2019-10-28 12:56:07 -0400

    adding server.js

--- src/app.js
+++ src/app.js
@@ -1,31 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
-
-const videos = require('./lib/videos');
-
-app.use((req, res, next) => {
- res.setHeader('Access-Control-Allow-Origin', '*');
- res.setHeader(
-  'Access-Control-Allow-Header',
-  'Origin, X-Requested-With, Content-Type, Accept'
- );
- res.setHeader(
-  'Access-Control-Allow-Methods',
-  'GET, POST, PATCH, DELETE, OPTIONS'
- );
- next();
-});
-
-app.use('/api/videos', (req, res, next) => {
- res.status(200).json({
-  message: 'Video Data fetched successfully!',
-  videos: videos
- });
-});
-
-app.use((req, res, next) => {
- res.send('Hello from Express');
-});
-
-module.exports = app;
